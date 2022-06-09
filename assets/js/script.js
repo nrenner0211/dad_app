e877381d058d23894626b12f3a8c62801f72d15b
christianmspence <103942566+christianmspence@users.noreply.github.com>
2022-06-08 23:35:08 -0400

    page is reloading upon click but new city info is not displaying

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,22 @@ let weather = {
 
     displayWeather: function (data) {
         var { name } = data;
+        var { icon, description } = data.weather[0];
         var { temp, temp_min, temp_max, humidity } = data.main;
-        console.log(name, temp, temp_min, temp_max, humidity);
+        console.log(name, icon, description, temp, temp_min, temp_max, humidity);
+
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".temp").innerText = temp + "°F";
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".highLow").innerText = "High: " + temp_max + "°F Low: " + temp_min + "°F";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+    },
+    search: function () {
+        this.fetchWeather(document.querySelector(".search-bar").value);
     }
 };
+
+document.querySelector(".card-body button").addEventListener("onClick", function () {
+    weather.search();
+});
