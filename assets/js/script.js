387207cf0ea149fc4cd85260bcb510cd6480a3f4
christianmspence <103942566+christianmspence@users.noreply.github.com>
2022-06-08 22:12:33 -0400

    pulling select info from API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,16 @@
 let weather = {
     myKey: "dcbc862bcf16c64fe76c5467e987cbd0",
-    fetchWeather: function () {
-        fetch("https://api.openweathermap.org/data/2.5/weather?q=" 
-        + city + "&units=imperial&appid=" + myKey
+    fetchWeather: function (city) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q="
+            + city + "&units=imperial&appid=" + this.myKey
         )
             .then((response) => response.json())
-            .then((data) => console.log(data));
+            .then((data) => this.displayWeather(data));
     },
 
+    displayWeather: function (data) {
+        var { name } = data;
+        var { temp, temp_min, temp_max, humidity } = data.main;
+        console.log(name, temp, temp_min, temp_max, humidity);
+    }
 };
