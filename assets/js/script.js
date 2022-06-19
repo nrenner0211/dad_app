089023c096c9118f9b97f68052aa31d471f55f1f
christianmspence <103942566+christianmspence@users.noreply.github.com>
2022-06-19 15:14:09 -0400

    display sports stats to container

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 // weather cookie - cameron cottman
-window.onload = function() {
+window.onload = function () {
     document.cookie.split(';').some((item) => {
         if (item.trim().startsWith("weather=")) {
             var lastSearch = item.trim().split('=')[1];
@@ -59,7 +59,7 @@ let weather = {
 //City search event listener -Christian
 document.querySelector("#search-button").addEventListener("click", function () {
     //  storing cookie -cameron
-    document.cookie = "weather="+document.querySelector(".search-bar").value; 
+    document.cookie = "weather=" + document.querySelector(".search-bar").value;
     // - Christian
     console.log("search button works");
     weather.search();
@@ -114,34 +114,49 @@ iconArray.addEventListener("mouseover", () => {
     iconArray.innerText = stormY[Math.floor(Math.random() * stormY.length)]
 });
 //end header emoji content -- niki
-	
+
 // Sports API start -- Ben
 const options = {
-	method: 'GET',
-	url: 'https://odds.p.rapidapi.com/v4/sports/baseball_mlb/scores',
-	params: {daysFrom: '365'},
-	headers: {
-	  'X-RapidAPI-Key': '94f7aacf8amsha7bbb052c8fd086p1916d8jsn6a414cca332f',
-	  'X-RapidAPI-Host': 'odds.p.rapidapi.com'
-	}
-  };
-  
-fetch('https://odds.p.rapidapi.com/v4/sports/baseball_mlb/scores', options)
-	.then(response => response.json())
-	.then(data => {
-		console.log(data)
-	var getHomeScore = data[0].scores[0]
-	var getAwayScore = data[0].scores[1]
-	displayScores(getHomeScore, getAwayScore);	
-	});
-
-function displayScores() {
-	document.querySelector(a).innertext = "";
+    method: 'GET',
+    url: 'https://odds.p.rapidapi.com/v4/sports/baseball_mlb/scores',
+    params: { daysFrom: '365' },
+    headers: {
+        'X-RapidAPI-Key': '94f7aacf8amsha7bbb052c8fd086p1916d8jsn6a414cca332f',
+        'X-RapidAPI-Host': 'odds.p.rapidapi.com'
+    }
 };
 
+var getSportsStats = function () {
+
+    fetch('https://odds.p.rapidapi.com/v4/sports/baseball_mlb/scores', options)
+        .then((response) => response.json())
+        .then((data) => displayScores(data))
+        .catch((error) => console.log(error));
+}
+
+//fetch('https://odds.p.rapidapi.com/v4/sports/baseball_mlb/scores', options)
+//.then(response => response.json())
+//.then(data => {
+// console.log(data)
+//var getHomeScore = data[0].scores[0]
+//var getAwayScore = data[0].scores[1]
+//displayScores(getHomeScore, getAwayScore);
+//});
+
+
+var displayScores = function (data) {
+    var getHomeScore = data[0].scores[0]
+    var getAwayScore = data[0].scores[1]
+
+    document.querySelector(".home-score").innerText = getHomeScore.name + "- " + getHomeScore.score;
+    document.querySelector(".away-score").innerText = getAwayScore.name + "- " + getAwayScore.score;
+    console.log(getHomeScore, getAwayScore);
+};
+
+getSportsStats();
 
 
 
-	
-	
+
+
 
